b3e0e25f8303f1ca9ba73202791429d6239b76bf
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2024-02-18 18:51:43 +0700

    mulai backend

--- models/paketModel.js
+++ models/paketModel.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
 
-const paketSchema = new mongoose.Schema({
-  title: String,
-  date: String,
-  price: String,
-  wa: String,
-  image: String,
-});
+const paketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "title is required"],
+    },
+    date: {
+      type: String,
+      required: [true, "date is required"],
+    },
+    price: {
+      type: String,
+      required: [true, "price is required"],
+    },
+    wa: {
+      type: String,
+      required: [true, "pesan is required"],
+    },
+    image: {
+      url: String,
+      public_id: String,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("paket", paketSchema);
---
+++ models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "first name is required"],
+      maxlength: 32,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: [true, "password is required"],
+      minlength: [6, "password must have at least (6) caracters"],
+    },
+  },
+  { timestamps: true }
+);
+
+//encrypting password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// compare user password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// return a JWT token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+    expiresIn: 3600,
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);

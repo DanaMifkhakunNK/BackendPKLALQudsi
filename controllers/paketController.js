4f40924a6c7ad469cce3831fa28aad8a1b85b25e
DanaMifkhakunNK <danamifkhakunnurkhasan@gmail.com>
2024-02-23 20:49:36 +0700

    done editpaket

--- controllers/paketController.js
+++ controllers/paketController.js
@@ -47,12 +47,63 @@ const getPaket = async (req, res, next) => {
 };
 // get paket  api/paket:id
 const getSingle = async (req, res, next) => {
-  res.json("Single paket");
+  try {
+    const paketId = req.params.id;
+    const paket = await Paket.findById(paketId);
+    if (!paket) {
+      return next(new HttpError("paket not found", 404));
+    }
+    res.status(200).json(paket);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
+
 // patch paket api/paket/:id
 const editPaket = async (req, res, next) => {
-  res.json("Edit Post");
+  try {
+    let fileName, newFilename, updatePaket;
+    const paketId = req.params.id;
+    let { judul, tanggal, harga, pesan } = req.body;
+
+    if (!req.files) {
+      updatePaket = await Paket.findByIdAndUpdate(paketId, { judul, tanggal, harga, pesan }, { new: true });
+    } else {
+      //get paket sebelumnya
+      const oldPaket = await Paket.findById(paketId);
+      //delete gambar sebelumnya
+      fs.unlink(path.join(__dirname, "..", "/uploads", oldPaket.gambar), async (err) => {
+        if (err) {
+          return next(new HttpError(err));
+        }
+      });
+      //upload gambar baru
+      const { gambar } = req.files;
+      if (gambar.size > 2000000) {
+        return next(new HttpError("ukuran gambar terlalu besar"));
+      }
+      let fileName = gambar.name;
+      let splittedFilename = fileName.split(".");
+      let newFilename = splittedFilename[0] + uuid() + "." + splittedFilename[splittedFilename.length - 1];
+      gambar.mv(path.join(__dirname, "..", "/uploads", newFilename), async (err) => {
+        if (err) {
+          return next(new HttpError(err));
+        }
+      });
+
+      updatePaket = await Paket.findByIdAndUpdate(paketId, { judul, tanggal, harga, pesan, gambar: newFilename }, { new: true });
+    }
+
+    if (!updatePaket) {
+      return next(new HtppError("tidak bsa update ", 400));
+    }
+
+    res.status(200).json(updatePaket);
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
+
 // detele paket api/paket/:id
 const deletePaket = async (req, res, next) => {
   res.json("delete Post");
